b4fc900a3f0cb6a603c3ee5c00fe20c8b43257a5
Ashley Scott <admin@projectge.com>
2017-04-26 17:22:26 +0100

    Functional RESTapi implementation

--- src/main/java/com/projectge/ci346/Application.java
+++ src/main/java/com/projectge/ci346/Application.java
@@ -2,17 +2,12 @@ package com.projectge.ci346;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class FrontEnd {
+public class Application {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(FrontEnd.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
-	@RequestMapping(value = "/")
-	public String index() {
-		return "index.html"; 
-	}
 }
--- src/main/java/com/projectge/ci346/BackEnd.java
+++ src/main/java/com/projectge/ci346/BackEnd.java
@@ -1,14 +1,30 @@
 package com.projectge.ci346;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BackEnd {
+
+	@RequestMapping(value = "/api", method = RequestMethod.GET)
+	public String GetIndex() {
+		return "{\"error\": 0, \"message\": \"Welcome to the server, you issued a GET request.\"}";
+	}
+
+	@RequestMapping(value = "/api", method = RequestMethod.POST)
+	public String PostIndex() {
+		return "{\"error\": 0, \"message\": \"Welcome to the server, you issued a POST request.\"}";
+	}
+
+	@RequestMapping(value = "/api", method = RequestMethod.PUT)
+	public String PutIndex() {
+		return "{\"error\": 0, \"message\": \"Welcome to the server, you issued a PUT request.\"}";
+	}
 	
-	@RequestMapping("/api")
-	public String IndexAPI() {
-		return "{\"error\": 0, \"message\": \"Welcome to the server.\"}";
+	@RequestMapping(value = "/api", method = RequestMethod.DELETE)
+	public String DeleteIndex() {
+		return "{\"error\": 0, \"message\": \"Welcome to the server, you issued a DELETE request.\"}";
 	}
 	
 }
