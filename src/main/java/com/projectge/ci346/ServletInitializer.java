69c231847f026eaaf2c0a0caee6bf185ed630323
Ashley Scott <admin@projectge.com>
2017-04-29 15:04:23 +0100

    Forgot to save this file, too

--- src/main/java/com/projectge/ci346/ServletInitializer.java
+++ src/main/java/com/projectge/ci346/ServletInitializer.java
@@ -3,6 +3,14 @@ package com.projectge.ci346;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * 
+ * @author Ashley Scott
+ * 
+ * Initialises the Spring application.
+ *
+ */
+
 public class ServletInitializer extends SpringBootServletInitializer {
 	
 	@Override
